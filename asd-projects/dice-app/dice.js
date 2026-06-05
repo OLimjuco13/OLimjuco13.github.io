54434e45116342095d8e1d5a05a3baf3da77dd9b
OLimjuco13 <olivier-elias_limjuco.stu@willowschoolnola.org>
2026-06-05 15:28:15 +0000

    finished die app w/o bonus

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,55 +1,67 @@
 $(document).ready(function () {
   // Your code goes here
-  $("<div>")
-  .css("height", 15)
-  .css("width", 15)
-  .css("background-color", "black")
-  .css("position", "absolute")
-  .css("top", 40)
-  .css("left", 40)
-  .appendTo("#die");
 
+  function makeDot(top, left, elementId) {
+    $("<div>")
+    .css("height", 15)
+    .css("width", 15)
+    .css("background-color", "black")
+    .css("position", "absolute")
+    .css("top", top)
+    .css("left", left)
+    .appendTo(elementId);
+}
 
- $("#die").on("click", handleClick);
+  $("#die").on("click", handleClick);
 });
-/* code above is the jQuery code: right now it's a .on for registering clicks
-and the dice's dot's css
-*/
-
 
 //Here I'm creating the functions to the dice game
 //This function matches the right code to the right face
-function makeDot(top, left, elementId) {
-    $("top")
+  function makeDot(top, left, elementId) {
+    $("<div>")
   .css("height", 15)
   .css("width", 15)
   .css("background-color", "black")
   .css("position", "absolute")
-  .css("top", 60)
-  .css("left", 50)
-  .appendTo("elementId");
-
-    $("bottom")
-  .css("height", 15)
-  .css("width", 15)
-  .css("background-color", "black")
-  .css("position", "absolute")
-  .css("top", 40)
-  .css("left", 50)
-  .appendTo("elementId");
-
-  $("elementId")
-    .appendTo("#die")
+  .css("top", top)
+  .css("left", left)
+  .appendTo(elementId);
 }
-
-makeDot(50, 50, "#die");
-makeDot(25, 25, "#die");
-makeDot(75, 75, "#die");
 
 //This logs the string "clicked" to console
 function rollDie(dieID) {
+  $(dieID).empty();
   var randomNum=Math.ceil(Math.random() * 6);
   console.log(randomNum);
+
+  if (randomNum === 1) {
+  makeDot(50, 50, dieID); // middle middle
+} else if (randomNum === 2) {
+  makeDot(25, 25, dieID); // top left
+  makeDot(75, 75, dieID); // bottom right
+} else if (randomNum === 3) {
+  makeDot(25, 25, dieID); // top left
+  makeDot(75, 75, dieID); // bottom right
+  makeDot(50, 50, dieID); // middle middle
+} else if (randomNum === 4) {
+  makeDot(75, 75, dieID); // bottom right
+  makeDot(25, 25, dieID); // top left
+  makeDot(25, 75, dieID); // bottom left
+  makeDot(75, 25, dieID); // top right
+} else if (randomNum === 5) {
+  makeDot(50, 50, dieID); // middle middle
+  makeDot(75, 75, dieID); // bottom right
+  makeDot(25, 25, dieID); // top left
+  makeDot(25, 75, dieID); // bottom left
+  makeDot(75, 25, dieID); // top right
+} else {
+  makeDot(25, 25, dieID);
+  makeDot(50, 25, dieID);
+  makeDot(75, 25, dieID);
+  makeDot(25, 75, dieID);
+  makeDot(50, 76, dieID); 
+  makeDot(75, 75, dieID);
+}
 }
 
 //This calls the rollDie function after a specific event occurs
