0f2d1cbf32594d81706c97de6af8dc2488ede160
OLimjuco13 <olivier-elias_limjuco.stu@willowschoolnola.org>
2026-06-24 16:58:52 +0000

    finalized html thru keydown

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -10,14 +10,37 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+  const KEY = {
+    ENTER: 13,
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40
+  }
+  const WASD = {
+    W: 87,
+    A: 65,
+    S: 83,
+    D: 68
+  }
   
   // Game Item Objects
-
+  function gameItem($id) {
+    var gameItem = {}
+    gameItem.id = $id;
+    gameItem.x = parseFloat($($id).css("left"));
+    gameItem.y = parseFloat($($id).css("top"));
+    gameItem.width = $($id).width();
+    gameItem.height = $($id).height();
+    gameItem.speedX = 0
+    gameItem.speedY = 0
+    return gameItem;
+  }
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);   
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -27,15 +50,42 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
 
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
+    console.log(event.which) 
 
+    if (event.which === KEY.ENTER ) {
+      console.log("ENTER pressed.");
+    } else if (event.which === KEY.LEFT) {
+      console.log("LEFT pressed.")
+    } else if (event.which === KEY.RIGHT) {
+      console.log("RIGHT pressed.")
+    } else if (event.which === KEY.UP) {
+      console.log("UP pressed.")
+      leftPaddle.speedY -= 5;
+    } else if (event.which === KEY.DOWN) {
+      console.log("DOWN pressed,")
+      leftPaddle.speedY += 5;
+    }
+
+    if (event.which === WASD.W) {
+      console.log("W pressed.");
+      rightPaddle.speedY -= 5;
+    } else if (event.which === WASD.A) {
+      console.log("A pressed.");
+    } else if (event.which === WASD.S) {
+      console.log("S pressed.");
+      rightPaddle.speedY += 5;
+    } else if (event.which === WASD.D) {
+      console.log("S pressed.")
+    } else if (event.which === WASD.ENTER) {
+      console.log("ENTER pressed.")
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
